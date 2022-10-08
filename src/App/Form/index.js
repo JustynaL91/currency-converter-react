b77ad20689e80c1b9e699e7fae42e386fe04c66f
Justyna Liwak <justyna.liwak91@gmail.com>
2022-10-08 19:26:38 +0200

    downloaded exchange rates from API

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import { Result } from "./Result";
-import { Button, Field, Header, Info, LabelText } from "./styled";
+import { Button, Field, Header, Info, LabelText, Loading, Failure } from "./styled";
+import { useRatesData } from "./useRatesData";
 
 
-export const Form = ({ calculateResult, result }) => {
-    const [currency, setCurrency] = useState(currencies[0].short);
+export const Form = () => {
+    const [result, setResult] = useState();
+    const ratesData = useRatesData();
+
+    const calculateResult = (currency, amount) => {
+        const rate = ratesData.rates[currency];
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount * rate,
+            currency,
+        });
+    }
+
+    const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
 
     const onSubmit = (event) => {
@@ -16,44 +29,59 @@ export const Form = ({ calculateResult, result }) => {
     return (
         <form onSubmit={onSubmit}>
             <Header>Przelicznik walut</Header>
-            <p>
-                <label>
-                    <LabelText>Kwota w PLN*: </LabelText>
-                    <Field
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        placeholder="Wpisz kwotę w zł"
-                        type="number"
-                        required step="0.01"
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <LabelText>Waluta: </LabelText>
-                    <Field
-                        as="select"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        )))}
+            {ratesData.state === "loading"
+                ? (
+                    <Loading>
+                        Sekundka... <br />Ładuję kursy walut z Europejskiego Banku Centralnego
+                    </Loading>
+                )
+                : (
+                    ratesData.state === "error" ? (
+                        <Failure>
+                            Coś poszło nie tak ! Sprawdź czy masz połączenie z internetem.
+                        </Failure>
+                    ) : (
+                        <>
+                            <p>
+                                <label>
+                                    <LabelText>Kwota w PLN*: </LabelText>
+                                    <Field
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                        placeholder="Wpisz kwotę w zł"
+                                        type="number"
+                                        required step="0.01"
+                                    />
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    <LabelText>Waluta: </LabelText>
+                                    <Field
+                                        as="select"
+                                        value={currency}
+                                        onChange={({ target }) => setCurrency(target.value)}
+                                    >
+                                        {Object.keys(ratesData.rates).map(((currency) => (
+                                            <option
+                                                key={currency}
+                                                value={currency}
+                                            >
+                                                {currency}
+                                            </option>
+                                        )))}
+                                    </Field>
+                                </label>
+                            </p>
+                            <p>
+                                <Button>Przelicz!</Button>
+                            </p>
+                            <Info>Kursy walut pobierane są z Europejskiego Banku Centralnego.<br /></Info>
 
-                    </Field>
-                </label>
-            </p>
-            <p>
-                <Button>Przelicz!</Button>
-            </p>
-            <Info>Kursy pochodzą ze strony nbp.pl z Tabeli nr 115/A/NBP/2021 z dnia 2021-06-16</Info>
-
-            <Result result={result} />
+                            <Result result={result} />
+                        </>
+                    )
+                )}
         </form>
-    )
-}
+    );
+};
